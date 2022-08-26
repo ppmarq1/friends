887e20becb8096d7f526bbc6ac047d075499fabd
Pedro Marques <ppmarq1@gmail.com>
2022-08-26 16:04:23 +0100

    Redirect routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'home/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  #get 'home/index'
+  root 'home#index'
 end
